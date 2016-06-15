1ebbc0d49212ac830590319eb2c911d3db2e9aeb
oShine <oyjqdlp@126.com>
2016-06-15 09:24:39 +0800

    测试file

--- system/modules/file/views/default/index.php
+++ system/modules/file/views/default/index.php
@@ -1,4 +1,4 @@
-<form action="<?php echo $this->createUrl("/file/default/upload");?>" method="post">
+<form action="<?php echo $this->createUrl("/file/default/upload");?>" method="post" enctype="multipart/form-data">
     <input type="file" name="file" class="dropify" data-height="300" data-max-file-size="2M"/>
     <input type="submit" class="btn btn-warning" value="确定">
 </form>
