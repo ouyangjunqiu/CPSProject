32c32280be4ce9256e16e09a9ccf4eb370be03b1
oShine <oyjqdlp@126.com>
2016-06-16 10:53:06 +0800

    实时报表与近期报表合并

--- system/modules/zuanshi/views/dashboard/index.php
+++ system/modules/zuanshi/views/dashboard/index.php
@@ -17,8 +17,7 @@
             <div class="search-left com-list-tit" style="display: block;">
                 <span class="shop-list-icon"></span>
                 <span class="shop-list-txt">智·钻</span><small>
-                    <a href="<?php echo $this->createUrl("/zuanshi/dashboard/index");?>"><span class="label label-info">实时状况</span></a>
-                    <a href="<?php echo $this->createUrl("/zuanshi/rpt/index");?>"><span class="label label-default">全店推广报表</span></a>
+                    <a href="<?php echo $this->createUrl("/zuanshi/dashboard/index");?>"><span class="label label-info">全店推广</span></a>
                     <a href="<?php echo $this->createUrl("/zuanshi/summary/index");?>"><span class="label label-default">店铺统计报表</span></a>
                     <a href="<?php echo $this->createUrl("/zuanshi/summary/pic");?>"><span class="label label-default">人员统计报表</span></a>
 
@@ -41,34 +40,59 @@
                     <?php $this->widget("application\\modules\\main\\widgets\\ShopManagerWidget",array("shop"=>$row));?>
                 </td>
                 <td class="check-infor-td">
-                    <div class="baby-box">
-                        <div class="baby-trusteeship baby-frame-box" data-nick="<?php echo $row["nick"];?>" data-role="list">
+                    <div class="baby-box" data-nick="<?php echo $row["nick"];?>" data-role="list">
+                        <ul class="nav nav-tabs shop-nav" role="tablist">
+                            <li role="presentation" class="active">
+                                <a data-type="realtime" href="#realtime_<?php echo $row["id"];?>" title="实时状况" aria-controls="realtime_<?php echo $row["id"];?>" role="tab" data-toggle="tab" aria-expanded="true">
+                                    <i class="fa fa-flash"></i><span>实时状况</span>
+                                </a>
+                                <a data-type="rpt" href="#rpt_<?php echo $row["id"];?>" title="历史报表" aria-controls="rpt_<?php echo $row["id"];?>" role="tab" data-toggle="tab" aria-expanded="true">
+                                    <i class="fa fa-bar-chart-o"></i><span>近期报表<small>(近15天)</small></span>
+                                </a>
+                                <a data-type="rpt" href="#rpt_chart_<?php echo $row["id"];?>" title="近期趋势" aria-controls="rpt_chart_<?php echo $row["id"];?>" role="tab" data-toggle="tab" aria-expanded="true">
+                                    <i class="fa fa-line-chart"></i><span>近期趋势</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
 
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <h3 class="baby-frame-h3">
-                                        <i class="tit-frame-icon"></i>
-                                        实时状况
-                                    </h3>
+                            <div role="tabpanel" class="tab-pane active" id="realtime_<?php echo $row["id"];?>">
+                                <div class="row" data-load="overlay" data-tmpl="shop-zuanshi-realtime-list-tmpl" data-role="shop-zuanshi-realtime-list" data-url="<?php echo $this->createUrl("/zuanshi/dashboard/getbynick",array("nick"=>$row["nick"]));?>" >
+
                                 </div>
-                                <div class="col-md-7">
+
+                                <div class="row padding" data-load="overlay" data-tmpl="shop-campaign-budget-warning-tmpl" data-url="<?php echo $this->createUrl("/zuanshi/dashboard/getcampaignbudgetwarning",array("nick"=>$row["nick"]));?>">
+
                                 </div>
-                                <div class="col-md-1">
+                                <div class="row">
+                                    <div class="col-md-11">
 
-                                    <small>
-                                        <a class="label label-primary" href="<?php echo $this->createUrl("/zuanshi/advertiser/more",array("nick"=>$row["nick"]));?>">详情</a>
-                                    </small>
-
+                                    </div>
+                                    <div class="col-md-1">
+                                        <small>
+                                            <a class="label label-primary" href="<?php echo $this->createUrl("/zuanshi/advertiser/more",array("nick"=>$row["nick"]));?>">详情</a>
+                                        </small>
+                                    </div>
                                 </div>
                             </div>
 
-                            <div class="row" data-load="overlay" data-tmpl="shop-zuanshi-realtime-list-tmpl" data-role="shop-zuanshi-realtime-list" data-url="<?php echo $this->createUrl("/zuanshi/dashboard/getbynick",array("nick"=>$row["nick"]));?>" >
+                            <div role="tabpanel" class="tab-pane" id="rpt_<?php echo $row["id"];?>">
+                                <div class="row" data-tmpl="shop-zuanshirpt-list-tmpl" data-load="overlay" data-url="<?php echo $this->createUrl("/zuanshi/rpt/getbynick",array("nick"=>$row["nick"],"shopname"=>$row["shopname"]));?>">
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-11">
+
+                                    </div>
+                                    <div class="col-md-1">
+                                        <small>
+                                            <a class="label label-primary" href="<?php echo $this->createUrl("/zuanshi/rpt/more",array("nick"=>$row["nick"]));?>">详情</a>
+                                        </small>
+                                    </div>
+                                </div>
 
                             </div>
 
-                            <div class="row padding" data-load="overlay" data-tmpl="shop-campaign-budget-warning-tmpl" data-url="<?php echo $this->createUrl("/zuanshi/dashboard/getcampaignbudgetwarning",array("nick"=>$row["nick"]));?>">
-
-                            </div>
 
                         </div>
                     </div>
@@ -370,6 +394,77 @@
 
 {{/if}}
 </script>
+
+<script type="text/x-jquery-tmpl" id="shop-zuanshirpt-list-tmpl">
+ {{if !isSuccess}}
+    <div>
+        <p class="text-danger">安装插件后，登录钻石展位即可同步展示！</p>
+    </div>
+    {{else}}
+    <table data-role="list" class="baby-frame-table" style="table-layout: fixed;">
+        <thead><tr class="small">
+            <th>日期</th>
+            <th>展现</th>
+            <th>点击</th>
+            <th>点击率</th>
+            <th>消耗</th>
+            <th>点击单价</th>
+            <th>收藏数</th>
+            <th>3天订单数</th>
+            <th>7天订单数</th>
+            <th>15天订单数</th>
+            <th>3天回报率</th>
+            <th>7天回报率</th>
+            <th>15天回报率</th>
+            <th>营业额</th>
+            <th>消耗占比(%)</th>
+        </tr>
+        </thead>
+        <tbody>
+        {{each(i,rpt) data.list}}
+            <tr class="small"><td><strong>${rpt.logDateStr}</strong></td>
+            <td>${rpt.adPv}</td>
+            <td>${rpt.click}</td>
+            <td>${rpt.ctrStr}</td>
+            <td>${rpt.charge}</td>
+            <td>${rpt.ecpc}</td>
+            <td>${rpt.favcount}</td>
+            <td>${rpt.alipayInShopNum}</td>
+            <td>${rpt.alipayInShopNum7}</td>
+            <td>${rpt.alipayInShopNum15}</td>
+            <td>${rpt.roi}</td>
+            <td>${rpt.roi7}</td>
+            <td>${rpt.roi15}</td>
+            <td>${rpt.payAmtStr}</td>
+            <td>${rpt.chargeRateStr}</td>
+            </tr>
+        {{/each}}
+
+        {{if data.total}}
+
+            <tr class="small"><td><strong>总计</strong></td>
+            <td>${data.total.adPv}</td>
+            <td>${data.total.click}</td>
+            <td>${data.total.ctrStr}</td>
+            <td>${data.total.charge}</td>
+            <td>${data.total.ecpc}</td>
+            <td>${data.total.favcount}</td>
+            <td>${data.total.alipayInShopNum}</td>
+            <td>${data.total.alipayInShopNum7}</td>
+            <td>${data.total.alipayInShopNum15}</td>
+            <td>${data.total.roi}</td>
+            <td>${data.total.roi7}</td>
+            <td>${data.total.roi15}</td>
+            <td>${data.total.payAmtStr}</td>
+            <td>${data.total.chargeRateStr}</td>
+            </tr>
+        {{/if}}
+        </tbody>
+
+        </table>
+{{/if}}
+</script>
+
 <script type="application/javascript">
 
     $(document).ready(function(){
@@ -381,7 +476,7 @@
             location.href = app.url("<?php echo $this->createUrl('/zuanshi/dashboard/index');?>",{page:dp.index+1})
         }});
 
-        $("[data-toggle=popover]").popover({html : true});
+        //$("[data-toggle=popover]").popover({html : true});
 
 
     });
