57a1e5613c73b86d78c08ff7bb088638df4bb9f0
oShine <oyjqdlp@126.com>
2016-07-27 16:05:34 +0800

    fixed

--- system/modules/zuanshi/views/rpt/index.php
+++ system/modules/zuanshi/views/rpt/index.php
@@ -19,7 +19,7 @@
                 <span class="shop-list-txt">智·钻</span><small>
                     <a href="<?php echo $this->createUrl("/zuanshi/dashboard/index");?>"><span class="label label-default">全店推广<small>(实时报表)</small></span></a>
                     <a href="<?php echo $this->createUrl("/zuanshi/rpt/index");?>"><span class="label label-info">全店推广<small>(近期报表)</small></span></a>
-                    <a href="<?php echo $this->createUrl("/zuanshi/rpt/index2");?>"><span class="label label-default">明星店铺<small>(近期报表)</small></span></a>
+<!--                    <a href="--><?php //echo $this->createUrl("/zuanshi/rpt/index2");?><!--"><span class="label label-default">明星店铺<small>(近期报表)</small></span></a>-->
                     <a href="<?php echo $this->createUrl("/zuanshi/summary/index");?>"><span class="label label-default">店铺统计报表</span></a>
                     <a href="<?php echo $this->createUrl("/zuanshi/summary/pic");?>"><span class="label label-default">人员统计报表</span></a>
 
