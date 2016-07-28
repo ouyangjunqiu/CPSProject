9a44927c4121243b75653398c1c2f67ab072076f
oShine <oyjqdlp@126.com>
2016-07-28 16:53:37 +0800

    fixed

---
+++ system/modules/zuanshi/model/AdvertiserRpt.php
@@ -0,0 +1,54 @@
+<?php
+namespace application\modules\zuanshi\model;
+
+use cloud\core\model\Model;
+
+
+class AdvertiserRpt extends Model
+{
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return AdvertiserRpt the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return '{{zuanshi_advertiser_rpt}}';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('nick', 'length', 'max' => 128),
+            array('logdate,effectType,effect,data', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('nick,logdate,effectType,effect,data', 'safe', 'on' => 'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array();
+    }
+
+
+}
