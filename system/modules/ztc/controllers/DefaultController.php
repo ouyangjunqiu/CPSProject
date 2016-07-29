3d3647935b2f3102c5431f06d38ba47d689b5cf2
oShine <oyjqdlp@126.com>
2016-07-29 14:40:37 +0800

    fixed

--- system/modules/ztc/controllers/DefaultController.php
+++ system/modules/ztc/controllers/DefaultController.php
@@ -1,6 +1,7 @@
 <?php
 namespace application\modules\ztc\controllers;
 use application\modules\main\model\Shop;
+use application\modules\main\utils\ShopSearch;
 use cloud\core\controllers\Controller;
 use cloud\core\utils\Env;
 
@@ -12,38 +13,10 @@ use cloud\core\utils\Env;
 class DefaultController extends Controller
 {
     public function actionIndex(){
-        $page = Env::getSession("page",1,"main.default.index");
-        $pageSize = Env::getSession("page_size",PAGE_SIZE,"main.default.index");
-        $q = Env::getSession("q","","main.default.index");
-        $view = Env::getRequest("view");
 
-        $page = (int)$page;
-        $pageSize = (int)$pageSize;
+        $data = ShopSearch::openlist();
 
-        $pic = Env::getSession("pic","","main.default.index");
-
-        $criteria = new \CDbCriteria();
-        $criteria->addCondition("status='0'");
-        if(!empty($q)) {
-            $criteria->addCondition("(nick LIKE '%{$q}%' OR pic LIKE '%{$q}%' OR zuanshi_pic LIKE '%{$q}%' OR bigdata_pic LIKE '%{$q}%' OR ztc_pic  LIKE '%{$q}%')");
-        }
-
-        if(!empty($pic)) {
-            $criteria->addCondition("(pic LIKE '%{$pic}%' OR zuanshi_pic LIKE '%{$pic}%' OR bigdata_pic LIKE '%{$pic}%' OR ztc_pic  LIKE '%{$pic}%')");
-        }
-
-        $count = Shop::model()->count($criteria);
-
-        $criteria->offset = ($page-1)*$pageSize;
-        $criteria->limit = $pageSize;
-
-        $list = Shop::model()->fetchAll($criteria);
-
-        if(strtolower($view) == "charts"){
-            $this->render("chart", array("list" => $list, "pager" => array("count" => $count, "page" => $page, "page_size" => $pageSize), "query" => array("q" => $q,"pic"=>$pic)));
-        }else {
-            $this->render("index", array("list" => $list, "pager" => array("count" => $count, "page" => $page, "page_size" => $pageSize), "query" => array("q" => $q,"pic"=>$pic)));
-        }
+        $this->render("index", $data);
     }
 
 }
