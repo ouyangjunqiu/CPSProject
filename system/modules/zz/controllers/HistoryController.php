aff1751977e0cb3888eab45ef733c4902abd8c6c
oShine <oyjqdlp@126.com>
2016-07-29 17:13:00 +0800

    fixed

--- system/modules/zz/controllers/HistoryController.php
+++ system/modules/zz/controllers/HistoryController.php
@@ -8,6 +8,9 @@
 namespace application\modules\zz\controllers;
 
 use application\modules\zz\model\AdboardRptHistory;
+use application\modules\zz\model\AdzoneRptHistory;
+use application\modules\zz\model\DestAdzoneRptHistory;
+use application\modules\zz\model\DestRptHistory;
 use cloud\core\controllers\Controller;
 use cloud\core\utils\Env;
 
@@ -25,6 +28,108 @@ class HistoryController extends Controller
 
         AdboardRptHistory::model()->deleteAll("logdate=? AND nick=? AND effectType=? AND effect=? AND offset=?",array($logdate,$nick,$effectType,$effect,$offset));
         $model = new AdboardRptHistory();
+        $model->setAttributes(array(
+            "nick"=>$nick,
+            "offset"=>$offset,
+            "effectType"=>$effectType,
+            "effect"=>$effect,
+            "logdate"=>$logdate,
+            "data"=>$data
+        ));
+
+        if($model->save()){
+            $this->renderJson(array(
+                "isSuccess"=>true,
+                "data"=>$model
+            ));
+        }else{
+            $this->renderJson(array(
+                "isSuccess"=>false,
+                "data"=>$model->getErrors()
+            ));
+        }
+    }
+
+    public function actionDest(){
+        $nick = Env::getRequest("nick");
+        $offset = Env::getRequest("offset");
+        $data = Env::getRequest("data");
+        $logdate = Env::getRequest("logdate");
+        $effectType = Env::getRequest("effectType");
+        $effect = Env::getRequest("effect");
+        $effectType = ($effectType=="click"?"click":"impression");
+        $nick = trim($nick);
+
+        DestRptHistory::model()->deleteAll("logdate=? AND nick=? AND effectType=? AND effect=? AND offset=?",array($logdate,$nick,$effectType,$effect,$offset));
+        $model = new DestRptHistory();
+        $model->setAttributes(array(
+            "nick"=>$nick,
+            "offset"=>$offset,
+            "effectType"=>$effectType,
+            "effect"=>$effect,
+            "logdate"=>$logdate,
+            "data"=>$data
+        ));
+
+        if($model->save()){
+            $this->renderJson(array(
+                "isSuccess"=>true,
+                "data"=>$model
+            ));
+        }else{
+            $this->renderJson(array(
+                "isSuccess"=>false,
+                "data"=>$model->getErrors()
+            ));
+        }
+    }
+
+    public function actionAdzone(){
+        $nick = Env::getRequest("nick");
+        $offset = Env::getRequest("offset");
+        $data = Env::getRequest("data");
+        $logdate = Env::getRequest("logdate");
+        $effectType = Env::getRequest("effectType");
+        $effect = Env::getRequest("effect");
+        $effectType = ($effectType=="click"?"click":"impression");
+        $nick = trim($nick);
+
+        AdzoneRptHistory::model()->deleteAll("logdate=? AND nick=? AND effectType=? AND effect=? AND offset=?",array($logdate,$nick,$effectType,$effect,$offset));
+        $model = new AdzoneRptHistory();
+        $model->setAttributes(array(
+            "nick"=>$nick,
+            "offset"=>$offset,
+            "effectType"=>$effectType,
+            "effect"=>$effect,
+            "logdate"=>$logdate,
+            "data"=>$data
+        ));
+
+        if($model->save()){
+            $this->renderJson(array(
+                "isSuccess"=>true,
+                "data"=>$model
+            ));
+        }else{
+            $this->renderJson(array(
+                "isSuccess"=>false,
+                "data"=>$model->getErrors()
+            ));
+        }
+    }
+
+    public function actionDestadzone(){
+        $nick = Env::getRequest("nick");
+        $offset = Env::getRequest("offset");
+        $data = Env::getRequest("data");
+        $logdate = Env::getRequest("logdate");
+        $effectType = Env::getRequest("effectType");
+        $effect = Env::getRequest("effect");
+        $effectType = ($effectType=="click"?"click":"impression");
+        $nick = trim($nick);
+
+        DestAdzoneRptHistory::model()->deleteAll("logdate=? AND nick=? AND effectType=? AND effect=? AND offset=?",array($logdate,$nick,$effectType,$effect,$offset));
+        $model = new DestAdzoneRptHistory();
         $model->setAttributes(array(
             "nick"=>$nick,
             "offset"=>$offset,
---
+++ system/modules/zz/model/DestRptHistory.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ouyangjunqiu
+ * Date: 16/4/14
+ * Time: 上午11:45
+ */
+
+namespace application\modules\zz\model;
+
+use cloud\core\model\Model;
+
+class DestRptHistory extends Model
+{
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return DestRptHistory the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return '{{zuanshi_dest_rpt_history_v2}}';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('logdate,nick,effectType,effect,offset,data', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('logdate,nick,effectType,effect,offset,data', 'safe', 'on' => 'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array();
+    }
+
+}
