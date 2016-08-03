4494966537758323765780b0d25badb3d374770b
oShine <oyjqdlp@126.com>
2016-08-03 18:04:47 +0800

    fixed

--- system/modules/main/widgets/views/todo.php
+++ system/modules/main/widgets/views/todo.php
@@ -107,12 +107,12 @@
                 {{if v.status==1}}
                     <a data-toggle="modal" data-target="#ShopTodoViewModal" data-backdrop="false" class="list-group-item list-group-item-success" data-content="${v.content}">
                     <small {{if v.priority=="紧急"}}class="danger"{{/if}}>[${v.priority}] ${v.creator}说:</small>{{html v.title}} {{if v.pic}}<small>@${v.pic}</small>{{/if}}
-                    <span class="badge label label-success"><i class="fa fa-clock-o"></i> ${v.days} days</small>
+                    <span class="label label-success"><i class="fa fa-clock-o"></i> ${v.days} days</small>
                     </a>
                 {{else}}
                    <a data-id="${v.id}" data-toggle="modal" data-target="#ShopTodoOpModal" data-backdrop="false" class="list-group-item list-group-item-danger" data-content="${v.content}" data-trigger-target="todo_${v.md5}">
                     <small {{if v.priority=="紧急"}}class="danger"{{/if}}>[${v.priority}] ${v.creator}说:</small>{{html v.title}} {{if v.pic}}<small>@${v.pic}</small>{{/if}}
-                    <span class="badge label label-danger"><i class="fa fa-clock-o"></i> ${v.days} days</small>
+                    <span class="label label-danger"><i class="fa fa-clock-o"></i> ${v.days} days</small>
                    </a>
                 {{/if}}
               {{/each}}
@@ -147,12 +147,12 @@
                {{if v.status==1}}
                     <a data-toggle="modal" data-target="#ShopTodoViewModal" data-backdrop="false" class="list-group-item list-group-item-success" data-content="${v.content}">
                     <small {{if v.priority=="紧急"}}class="danger"{{/if}}>[${v.priority}] ${v.creator}说:</small>{{html v.title}} {{if v.pic}}<small>@${v.pic}</small>{{/if}}
-                     <small class="badge label label-success"><i class="fa fa-clock-o"></i> ${v.days} days</small>
+                     <small class="label label-success"><i class="fa fa-clock-o"></i> ${v.days} days</small>
                     </a>
                 {{else}}
                    <a data-id="${v.id}" data-toggle="modal" data-target="#ShopTodoOpModal" data-backdrop="false" class="list-group-item" data-content="${v.content}"  data-trigger-target="todo_${v.md5}">
                     <small {{if v.priority=="紧急"}}class="danger"{{/if}}>[${v.priority}] ${v.creator}说:</small>{{html v.title}} {{if v.pic}}<small>@${v.pic}</small>{{/if}}
-                    <small class="badge label label-success"><i class="fa fa-clock-o"></i> ${v.days} days</small>
+                    <small class="label label-success"><i class="fa fa-clock-o"></i> ${v.days} days</small>
                    </a>
                 {{/if}}
               {{/each}}
