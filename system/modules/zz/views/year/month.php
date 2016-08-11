f356e200a5f7adc2f02ec64a2da99d00ea590631
oShine <oyjqdlp@126.com>
2016-08-11 16:51:35 +0800

    fixed

--- system/modules/zz/views/year/month.php
+++ system/modules/zz/views/year/month.php
@@ -21,7 +21,7 @@
                         <small>
                             <!--                            <a href="--><?php //echo $this->createUrl("/zuanshi/advertiser/more",array("nick"=>$query["nick"]));?><!--"><span class="label label-default">实时状况</span></a>-->
                             <a href="<?php echo $this->createUrl("/zz/advertiserrpt/more",array("nick"=>$query["nick"]));?>"><span class="label label-default">全店推广报表</span></a>
-                            <a href="<?php echo $this->createUrl("/zz/year/month",array("nick"=>$query["nick"]));?>"><span class="label label-default">年度走势</span></a>
+                            <a href="<?php echo $this->createUrl("/zz/year/month",array("nick"=>$query["nick"]));?>"><span class="label label-info">年度走势</span></a>
                             <!--                            <a href="--><?php //echo $this->createUrl("/zuanshi/adboard/index",array("nick"=>$query["nick"]));?><!--"><span class="label label-default">创意优选</span></a>-->
                             <!--                            <a href="--><?php //echo $this->createUrl("/zuanshi/dest/index",array("nick"=>$query["nick"]));?><!--"><span class="label label-default">定向优选</span></a>-->
                             <!--                            <a href="--><?php //echo $this->createUrl("/zuanshi/adzonerpt/index",array("nick"=>$query["nick"]));?><!--"><span class="label label-default">资源位优选</span></a>-->
@@ -79,7 +79,7 @@ for($i=1;$i<$month;$i++){
 
         var c1 = {
             chart: {
-                height: 500,
+                height: 400,
                 width: 500
             },
             title: {
@@ -137,7 +137,7 @@ for($i=1;$i<$month;$i++){
 
         var c2 = {
             chart: {
-                height: 500,
+                height: 400,
                 width: 500
             },
             title: {
@@ -179,7 +179,7 @@ for($i=1;$i<$month;$i++){
             series: [
                 {
                     type: 'spline',
-                    name: '消耗',
+                    name: '转化金额',
                     yAxis: 0,
                     data: <?php echo CJavaScript::jsonEncode($y_amt);?>,
                     marker: {
