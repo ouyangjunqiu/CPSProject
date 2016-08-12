47e27840ac92cf67d34d11c79ab04ffeb1b952ac
oShine <oyjqdlp@126.com>
2016-08-12 12:16:12 +0800

    fixed

--- system/modules/zz/views/year/month.php
+++ system/modules/zz/views/year/month.php
@@ -73,9 +73,9 @@ $y_amt = array();
 $y_trade = array();
 for($i=1;$i<$month;$i++){
     $x[] = $i;
-    $y_charge[] = isset($data[$i])?$data[$i]["charge"]:0;
-    $y_amt[] = isset($data[$i])?round($data[$i]["alipayInshopAmt"],2):0;
-    $y_trade[] = isset($trade[$i])?round($trade[$i],2):0;
+    $y_charge[] = isset($data[$i])?$data[$i]["charge"]:null;
+    $y_amt[] = isset($data[$i])?round($data[$i]["alipayInshopAmt"],2):null;
+    $y_trade[] = isset($trade[$i])?round($trade[$i],2):null;
 }
 ?>
 
