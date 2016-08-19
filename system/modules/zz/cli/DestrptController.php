e1168604fbc72e6127e5fc011f126b7492d6def1
oShine <oyjqdlp@126.com>
2016-08-19 17:40:34 +0800

    fixed

---
+++ system/modules/zz/cli/DestrptController.php
@@ -0,0 +1,220 @@
+<?php
+namespace application\modules\zz\cli;
+use application\modules\main\model\Shop;
+use application\modules\zz\model\DestE15Rpt;
+use application\modules\zz\model\DestE3Rpt;
+use application\modules\zz\model\DestE7Rpt;
+use application\modules\zz\model\DestRptHistory;
+use cloud\core\cli\Controller;
+
+/**
+ * @file AdboardrptController.php
+ * @author ouyangjunqiu
+ * @version Created by 16/8/16 09:41
+ */
+class DestrptController extends Controller
+{
+    public function actionE3(){
+        $fields = array(
+            "campaignId",
+            "campaignName",
+            "transId",
+            "transName",
+            "targetId",
+            "targetName",
+            "destType",
+            "adPv",
+            "alipayInShopNum",
+            "alipayInshopAmt",
+            "avgAccessPageNum",
+            "avgAccessTime",
+            "cartNum",
+            "charge",
+            "click",
+            "ctr",
+            "cvr",
+            "deepInshopUv",
+            "dirShopColNum",
+            "ecpc",
+            "ecpm",
+            "gmvInshopAmt",
+            "gmvInshopNum",
+            "inshopItemColNum",
+            "roi",
+            "uv"
+        );
+
+        for($i=6;$i>3;$i--){
+            $logdate = date('Y-m-d',strtotime("-$i days"));
+            $criteria = new \CDbCriteria();
+            $criteria->addCondition("status='0'");
+            $shops = Shop::model()->fetchAll($criteria);
+            foreach($shops as $shop){
+                $sources = DestRptHistory::model()->fetchAll("logdate=? AND nick=? AND effectType=? AND effect=?",array($logdate,$shop["nick"],"click",3));
+                if(empty($sources))
+                    continue;
+                foreach($sources as $source){
+                    $data =  \CJSON::decode($source["data"]);
+                    if(!empty($data) && !empty($data["result"])){
+
+                        foreach($data["result"] as $row){
+                            $rpt = array(
+                                "logdate"=>$logdate,
+                                "nick"=>$shop["nick"]
+                            );
+                            foreach($fields as $f){
+                                $rpt[$f] = $row[$f];
+                            }
+
+                            DestE3Rpt::model()->deleteAll("logdate=? AND nick=? AND campaignId=? AND transId=? AND targetId=?",array($rpt["logdate"],$rpt["nick"],$rpt["campaignId"],$rpt["transId"],$rpt["targetId"]));
+                            $model = new DestE3Rpt();
+                            $model->setAttributes($rpt);
+                            if(!$model->save()){
+                                print_r($model->getErrors());
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+
+
+    public function actionE7(){
+        $fields = array(
+            "campaignId",
+            "campaignName",
+            "transId",
+            "transName",
+            "targetId",
+            "targetName",
+            "destType",
+            "adPv",
+            "alipayInShopNum",
+            "alipayInshopAmt",
+            "avgAccessPageNum",
+            "avgAccessTime",
+            "cartNum",
+            "charge",
+            "click",
+            "ctr",
+            "cvr",
+            "deepInshopUv",
+            "dirShopColNum",
+            "ecpc",
+            "ecpm",
+            "gmvInshopAmt",
+            "gmvInshopNum",
+            "inshopItemColNum",
+            "roi",
+            "uv"
+        );
+
+        for($i=9;$i>6;$i--){
+            $logdate = date('Y-m-d',strtotime("-$i days"));
+            $criteria = new \CDbCriteria();
+            $criteria->addCondition("status='0'");
+            $shops = Shop::model()->fetchAll($criteria);
+            foreach($shops as $shop){
+                $sources = DestRptHistory::model()->fetchAll("logdate=? AND nick=? AND effectType=? AND effect=?",array($logdate,$shop["nick"],"click",3));
+                if(empty($sources))
+                    continue;
+                foreach($sources as $source){
+                    $data =  \CJSON::decode($source["data"]);
+                    if(!empty($data) && !empty($data["result"])){
+
+                        foreach($data["result"] as $row){
+                            $rpt = array(
+                                "logdate"=>$logdate,
+                                "nick"=>$shop["nick"]
+                            );
+                            foreach($fields as $f){
+                                $rpt[$f] = $row[$f];
+                            }
+
+                            DestE7Rpt::model()->deleteAll("logdate=? AND nick=? AND campaignId=? AND transId=? AND targetId=?",array($rpt["logdate"],$rpt["nick"],$rpt["campaignId"],$rpt["transId"],$rpt["targetId"]));
+                            $model = new DestE7Rpt();
+                            $model->setAttributes($rpt);
+                            if(!$model->save()){
+                                print_r($model->getErrors());
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    public function actionE15(){
+        $fields = array(
+            "campaignId",
+            "campaignName",
+            "transId",
+            "transName",
+            "targetId",
+            "targetName",
+            "destType",
+            "adPv",
+            "alipayInShopNum",
+            "alipayInshopAmt",
+            "avgAccessPageNum",
+            "avgAccessTime",
+            "cartNum",
+            "charge",
+            "click",
+            "ctr",
+            "cvr",
+            "deepInshopUv",
+            "dirShopColNum",
+            "ecpc",
+            "ecpm",
+            "gmvInshopAmt",
+            "gmvInshopNum",
+            "inshopItemColNum",
+            "roi",
+            "uv"
+        );
+
+        for($i=17;$i>14;$i--){
+            $logdate = date('Y-m-d',strtotime("-$i days"));
+            $criteria = new \CDbCriteria();
+            $criteria->addCondition("status='0'");
+            $shops = Shop::model()->fetchAll($criteria);
+            foreach($shops as $shop){
+                $sources = DestRptHistory::model()->fetchAll("logdate=? AND nick=? AND effectType=? AND effect=?",array($logdate,$shop["nick"],"click",3));
+                if(empty($sources))
+                    continue;
+                foreach($sources as $source){
+                    $data =  \CJSON::decode($source["data"]);
+                    if(!empty($data) && !empty($data["result"])){
+
+                        foreach($data["result"] as $row){
+                            $rpt = array(
+                                "logdate"=>$logdate,
+                                "nick"=>$shop["nick"]
+                            );
+                            foreach($fields as $f){
+                                $rpt[$f] = $row[$f];
+                            }
+
+                            DestE15Rpt::model()->deleteAll("logdate=? AND nick=? AND campaignId=? AND transId=? AND targetId=?",array($rpt["logdate"],$rpt["nick"],$rpt["campaignId"],$rpt["transId"],$rpt["targetId"]));
+                            $model = new DestE15Rpt();
+                            $model->setAttributes($rpt);
+                            if(!$model->save()){
+                                print_r($model->getErrors());
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+
+
+}
---
+++ system/modules/zz/model/DestE7Rpt.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ouyangjunqiu
+ * Date: 16/4/14
+ * Time: 上午11:45
+ */
+
+namespace application\modules\zz\model;
+
+use cloud\core\model\Model;
+
+class DestE7Rpt extends Model
+{
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return DestE7Rpt the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return '{{zuanshi_dest_d_e7_rpt}}';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('logdate,nick,campaignId,campaignName,transId,transName,targetId,targetName,destType,adPv,alipayInShopNum,alipayInshopAmt,avgAccessPageNum,avgAccessTime,cartNum,charge,click,ctr,cvr,deepInshopUv,dirShopColNum,ecpc,ecpm,gmvInshopAmt,gmvInshopNum,inshopItemColNum,roi,uv', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('logdate,nick,campaignId,campaignName,transId,transName,targetId,targetName,destType,adPv,alipayInShopNum,alipayInshopAmt,avgAccessPageNum,avgAccessTime,cartNum,charge,click,ctr,cvr,deepInshopUv,dirShopColNum,ecpc,ecpm,gmvInshopAmt,gmvInshopNum,inshopItemColNum,roi,uv', 'safe', 'on' => 'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array();
+    }
+
+
+}
