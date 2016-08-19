87c708f0932c886c3874f0cc15d1c30a3dafd586
oShine <oyjqdlp@126.com>
2016-08-19 17:02:49 +0800

    fixed

--- system/modules/zz/cli/AdboardrptController.php
+++ system/modules/zz/cli/AdboardrptController.php
@@ -1,7 +1,9 @@
 <?php
 namespace application\modules\zz\cli;
 use application\modules\main\model\Shop;
+use application\modules\zz\model\AdboardE15Rpt;
 use application\modules\zz\model\AdboardE3Rpt;
+use application\modules\zz\model\AdboardE7Rpt;
 use application\modules\zz\model\AdboardRptHistory;
 use application\modules\zz\model\AdboardWeekRpt;
 use cloud\core\cli\Controller;
@@ -66,6 +68,138 @@ class AdboardrptController extends Controller
 
                             AdboardE3Rpt::model()->deleteAll("logdate=? AND nick=? AND campaignId=? AND transId=? AND adboardId=?",array($rpt["logdate"],$rpt["nick"],$rpt["campaignId"],$rpt["transId"],$rpt["adboardId"]));
                             $model = new AdboardE3Rpt();
+                            $model->setAttributes($rpt);
+                            if(!$model->save()){
+                                print_r($model->getErrors());
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    public function actionE7(){
+        $fields = array(
+            "campaignId",
+            "campaignName",
+            "transId",
+            "transName",
+            "adboardId",
+            "adboardName",
+            "adPv",
+            "alipayInShopNum",
+            "alipayInshopAmt",
+            "avgAccessPageNum",
+            "avgAccessTime",
+            "cartNum",
+            "charge",
+            "click",
+            "ctr",
+            "cvr",
+            "deepInshopUv",
+            "dirShopColNum",
+            "ecpc",
+            "ecpm",
+            "gmvInshopAmt",
+            "gmvInshopNum",
+            "inshopItemColNum",
+            "roi",
+            "uv"
+        );
+
+        for($i=9;$i>6;$i--){
+            $logdate = date('Y-m-d',strtotime("-$i days"));
+            $criteria = new \CDbCriteria();
+            $criteria->addCondition("status='0'");
+            $shops = Shop::model()->fetchAll($criteria);
+            foreach($shops as $shop){
+                $sources = AdboardRptHistory::model()->fetchAll("logdate=? AND nick=? AND effectType=? AND effect=?",array($logdate,$shop["nick"],"click",7));
+                if(empty($sources))
+                    continue;
+                foreach($sources as $source){
+                    $data =  \CJSON::decode($source["data"]);
+                    if(!empty($data) && !empty($data["result"])){
+
+                        foreach($data["result"] as $row){
+                            $rpt = array(
+                                "logdate"=>$logdate,
+                                "nick"=>$shop["nick"]
+                            );
+                            foreach($fields as $f){
+                                $rpt[$f] = $row[$f];
+                            }
+
+                            AdboardE7Rpt::model()->deleteAll("logdate=? AND nick=? AND campaignId=? AND transId=? AND adboardId=?",array($rpt["logdate"],$rpt["nick"],$rpt["campaignId"],$rpt["transId"],$rpt["adboardId"]));
+                            $model = new AdboardE7Rpt();
+                            $model->setAttributes($rpt);
+                            if(!$model->save()){
+                                print_r($model->getErrors());
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    public function actionE15(){
+        $fields = array(
+            "campaignId",
+            "campaignName",
+            "transId",
+            "transName",
+            "adboardId",
+            "adboardName",
+            "adPv",
+            "alipayInShopNum",
+            "alipayInshopAmt",
+            "avgAccessPageNum",
+            "avgAccessTime",
+            "cartNum",
+            "charge",
+            "click",
+            "ctr",
+            "cvr",
+            "deepInshopUv",
+            "dirShopColNum",
+            "ecpc",
+            "ecpm",
+            "gmvInshopAmt",
+            "gmvInshopNum",
+            "inshopItemColNum",
+            "roi",
+            "uv"
+        );
+
+        for($i=17;$i>15;$i--){
+            $logdate = date('Y-m-d',strtotime("-$i days"));
+            $criteria = new \CDbCriteria();
+            $criteria->addCondition("status='0'");
+            $shops = Shop::model()->fetchAll($criteria);
+            foreach($shops as $shop){
+                $sources = AdboardRptHistory::model()->fetchAll("logdate=? AND nick=? AND effectType=? AND effect=?",array($logdate,$shop["nick"],"click",15));
+                if(empty($sources))
+                    continue;
+                foreach($sources as $source){
+                    $data =  \CJSON::decode($source["data"]);
+                    if(!empty($data) && !empty($data["result"])){
+
+                        foreach($data["result"] as $row){
+                            $rpt = array(
+                                "logdate"=>$logdate,
+                                "nick"=>$shop["nick"]
+                            );
+                            foreach($fields as $f){
+                                $rpt[$f] = $row[$f];
+                            }
+
+                            AdboardE15Rpt::model()->deleteAll("logdate=? AND nick=? AND campaignId=? AND transId=? AND adboardId=?",array($rpt["logdate"],$rpt["nick"],$rpt["campaignId"],$rpt["transId"],$rpt["adboardId"]));
+                            $model = new AdboardE15Rpt();
                             $model->setAttributes($rpt);
                             if(!$model->save()){
                                 print_r($model->getErrors());
