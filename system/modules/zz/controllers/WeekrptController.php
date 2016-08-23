a9d9bbad9373fc1d2afbe14e1b35058de69d97d3
oShine <oyjqdlp@126.com>
2016-08-23 09:13:46 +0800

    fixed

--- system/modules/zz/controllers/WeekrptController.php
+++ system/modules/zz/controllers/WeekrptController.php
@@ -18,10 +18,21 @@ class WeekrptController extends Controller
         $orderby = Env::getSession("orderby","charge","zuanshi.weekrpt.index");
         $date = Env::getSession("date",date("Y-m-d"),"zuanshi.weekrpt.index");
 
+        $w  = date('w',strtotime($date));
+        $now_start = date('Y-m-d',strtotime("$date -".($w ? $w - 1 : 6).' days')); //获取本周开始日期，如果$w是0，则表示周日，减去 6 天
+
+        $begindate = date('Y-m-d',strtotime("$now_start -7 days"));  //上周开始日期
+        $enddate = date('Y-m-d',strtotime("$begindate + 6 days"));  //上周结束日期
+
+        $nick = trim($nick);
+
+
         $this->render("index",array("query"=>array(
             "nick"=>$nick,
             "orderby"=>$orderby,
-            "date"=>$date
+            "date"=>$date,
+            "begindate"=>$begindate,
+            "enddate"=>$enddate
         )));
 
     }
