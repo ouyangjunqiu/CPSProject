3c683b685152077ad6dfe6877ffd5bd68561520e
oShine <oyjqdlp@126.com>
2016-09-19 14:35:58 +0800

    fixed

--- system/modules/main/widgets/views/shop.php
+++ system/modules/main/widgets/views/shop.php
@@ -124,7 +124,13 @@
                             <div class="col-md-4"><small>服务周期:</small></div>
                             <div class="col-md-8">
                                 <?php if(!empty($row["enddate"])):?>
+                                    <?php $days = ceil((strtotime($row["enddate"])-strtotime(date("Y-m-d")))/3600/24);?>
+                                    <?php if($days>=0):?>
+                                        <small>剩余</small><strong><?php echo $days;?></strong><small>天</small>
 
+                                        <small>(<?php echo empty($row["startdate"])?"":date("Y.m.d",$row["startdate"]);?>~<?php echo date("Y.m.d",$row["enddate"]);?>)</small>
+
+                                    <?php endif;?>
 
                                 <?php endif;?>
                             </div>
