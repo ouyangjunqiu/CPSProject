5a7d7e4e4b294ca5430d40fcdefc7747ab296865
oShine <oyjqdlp@126.com>
2016-09-19 09:13:22 +0800

    fixed

--- system/modules/main/widgets/views/shop.php
+++ system/modules/main/widgets/views/shop.php
@@ -313,10 +313,10 @@
                             <div class="col-md-3"><small>淘宝密码:</small></div>
                             <div class="col-md-7">
                                 <span class="pic_read">
-                                    <strong><?php echo $row["login_password"];?></strong>
+                                    <strong><?php echo str_repeat("*",strlen($row["login_password"]));?></strong>
                                 </span>
                                 <span class="pic_input form_writer" style="display: none">
-                                    <input type="text" value="<?php echo $row["login_password"];?>" name="login_password"/>
+                                    <input type="password" value="<?php echo $row["login_password"];?>" name="login_password"/>
                                 </span>
                             </div>
                             <div class="col-md-2">
