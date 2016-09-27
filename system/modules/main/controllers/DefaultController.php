bbe909afb79a1c69745b3178bfb9482ddff6b7f4
oShine <oyjqdlp@126.com>
2016-09-27 15:09:29 +0800

    fixed

--- system/modules/main/controllers/DefaultController.php
+++ system/modules/main/controllers/DefaultController.php
@@ -35,8 +35,8 @@ class DefaultController extends Controller
 	}
 
 	/**
-	 * 暂停的店铺列表
-	 */
+ * 暂停的店铺列表
+ */
 	public function actionStoplist(){
 		$page = Env::getSession("page",1,"main.default.index");
 		$pageSize = Env::getSession("page_size",PAGE_SIZE,"main.default.index");
@@ -73,6 +73,47 @@ class DefaultController extends Controller
 		$list = Shop::model()->fetchAll($criteria);
 
 		$this->render("stoplist",array("list"=>$list,"pager"=>array("count"=>$count,"page"=>$page,"page_size"=>$pageSize),"query"=>array("q"=>$q,"pic"=>$pic)));
+	}
+
+	/**
+	 * 暂停的店铺列表
+	 */
+	public function actionBeinglost(){
+		$page = Env::getSession("page",1,"main.default.index");
+		$pageSize = Env::getSession("page_size",PAGE_SIZE,"main.default.index");
+		$q = Env::getSession("q","","main.default.index");
+		$pic = Env::getSession("pic","","main.default.index");
+		$status = Env::getRequest("status");
+		$page = (int)$page;
+		$pageSize = (int)$pageSize;
+		$shoptype = Env::getRequest("shoptype");
+
+		$criteria = new \CDbCriteria();
+		if(!empty($status)){
+			$status = (int)$status;
+			$criteria->addCondition("status='{$status}'");
+		}else{
+			$criteria->addInCondition("status",array(1));
+		}
+
+		if(!empty($pic)) {
+			$criteria->addCondition("(pic LIKE '%{$pic}%' OR zuanshi_pic LIKE '%{$pic}%' OR bigdata_pic LIKE '%{$pic}%' OR ztc_pic  LIKE '%{$pic}%')");
+		}
+		if(!empty($q)) {
+			$criteria->addCondition("(shopcatname LIKE '%{$q}%' OR shoptype LIKE '%{$q}%' OR nick LIKE '%{$q}%' OR pic LIKE '%{$q}%' OR zuanshi_pic LIKE '%{$q}%' OR bigdata_pic LIKE '%{$q}%' OR ztc_pic  LIKE '%{$q}%')");
+		}
+		if(!empty($shoptype)) {
+			$criteria->addCondition("shoptype = '{$shoptype}'");
+		}
+
+		$count = Shop::model()->count($criteria);
+
+		$criteria->offset = ($page-1)*$pageSize;
+		$criteria->limit = $pageSize;
+
+		$list = Shop::model()->fetchAll($criteria);
+
+		$this->render("beginglost",array("list"=>$list,"pager"=>array("count"=>$count,"page"=>$page,"page_size"=>$pageSize),"query"=>array("q"=>$q,"pic"=>$pic)));
 	}
 
 
---
+++ system/modules/main/views/default/beginglost.php
@@ -0,0 +1,57 @@
+<script src='<?php echo STATICURL."/main/js/shop.js"; ?>'></script>
+
+<link rel="stylesheet" href="<?php echo STATICURL.'/main/css/index.css'; ?>">
+
+<div class="index-table-div">
+
+    <div class="search-box">
+        <div class="shop-list-cont" id="shop-search">
+            <div class="search-left com-list-tit" style="display: block;">
+                <span class="shop-list-icon"></span>
+                <span class="shop-list-txt">我的店铺</span>
+                <small>
+                    <a href="<?php echo $this->createUrl("/main/shop/index");?>"><span class="label label-default">新增店铺</span></a>
+                    <a href="<?php echo $this->createUrl("/main/default/index");?>"><span class="label label-default">我的店铺</span></a>
+                    <a href="<?php echo $this->createUrl("/main/default/beginglost");?>"><span class="label label-info">当月流失店铺</span></a>
+                    <a href="<?php echo $this->createUrl("/main/default/stoplist");?>"><span class="label label-default">流失店铺</span></a>
+                    <a href="<?php echo $this->createUrl("/main/dashboard/index");?>"><span class="label label-default">总览</span></a>
+
+                </small>
+            </div>
+            <div class="search-right">
+                <?php $this->widget("application\\modules\\main\\widgets\\ShopSearchWidget",array("url"=>$this->createUrl("/main/default/beginglost",array("page"=>1)),"query"=>$query));?>
+            </div>
+        </div>
+    </div>
+    <table>
+        <tbody>
+        <?php foreach($list as $row):?>
+            <tr>
+                <td class="babyInforTb-td-left">
+                    <?php $this->widget("application\\modules\\main\\widgets\\ShopStopManagerWidget",array("shop"=>$row));?>
+                </td>
+            </tr>
+        <?php endforeach;?>
+        </tbody>
+    </table>
+    <div class="c-pager">
+    </div>
+</div>
+
+<?php $this->widget("application\\modules\\main\\widgets\\ShopTodoWidget");?>
+<script type="application/javascript">
+
+    $(document).ready(function(){
+
+        $(".top-ul>li").eq(0).addClass("top-li-hover");
+
+
+        $(".c-pager").jPager({currentPage: <?php echo $pager["page"]-1;?>, total: <?php echo $pager["count"];?>, pageSize: <?php echo $pager["page_size"];?>,events: function(dp){
+            location.href = app.url("<?php echo $this->createUrl('/main/default/beginglost');?>",{page:dp.index+1})
+        }});
+
+    });
+
+
+</script>
+
