cb3ebd6937dba91df39f0484ef98637c96b79b87
oShine <oyjqdlp@126.com>
2016-10-12 15:12:54 +0800

    fixed

--- system/modules/main/widgets/views/search.php
+++ system/modules/main/widgets/views/search.php
@@ -1,26 +1,18 @@
 
 <form id="shop-search-form" action="<?php echo $url;?>" method="get" class="form-inline">
-    <div class="input-group">
-        <a href="javascript:void(0)" data-click="my-shop" data-user="<?php echo $user["username"];?>">
-
-            <?php if(!empty($query["pic"]) && $query["pic"] == $user["username"]):?>
-                <span class="label label-warning"><i class="glyphicon glyphicon-star" aria-hidden="true">取消展示我的</i>
-            <?php else:?>
-                <span class="label label-default"><i class="glyphicon glyphicon-star-empty" aria-hidden="true">仅展示我的</i>
-                </span>
-            <?php endif;?>
-        </a>
-    </div>
 
     <div class="btn-group">
-        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" style="border-color: white">
             <span class="glyphicon glyphicon-filter" style="color: #428bca"></span>
             <?php echo empty($query["pic"])?"所有":"@".$query["pic"];?>
             <span class="caret"></span>
         </button>
         <ul class="dropdown-menu" role="menu">
-            <li><a href="#">所有</a></li>
-            <li><a href="#">@<?php echo $user["username"];?></a></li>
+            <?php if(!empty($query["pic"])):?>
+                <li><a href="<?php echo $url;?>">所有</a></li>
+            <?php else:?>
+                <li><a href="<?php echo $url."&page=1&pic=".$user["username"];?>">@<?php echo $user["username"];?></a></li>
+            <?php endif;?>
         </ul>
     </div>
     <div class="input-group">
@@ -50,17 +42,6 @@
             data.q = form.find("input[name=q]").val();
             location.href = app.url(form.attr("action"),data);
         });
-
-        $("#shop-search-form [data-click=my-shop]").click(function(){
-            var form = $(this).parents("#shop-search-form");
-            var self = $(this);
-            var data = {};
-            if(self.find(".glyphicon-star-empty").size()>0){
-                data.pic = self.data("user");
-            }else{
-                data.pic = "";
-            }
-            location.href = app.url(form.attr("action"),data);
-        });
+        
     })
 </script>
