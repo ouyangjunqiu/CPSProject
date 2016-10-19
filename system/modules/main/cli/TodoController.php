8d6f5f3ea85d33f31dd94ac6acba8743a9a61359
oShine <oyjqdlp@126.com>
2016-10-19 11:55:17 +0800

    fixed

---
+++ system/modules/main/cli/TodoController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @file TodoController.php
+ * @author ouyangjunqiu
+ * @version Created by 2016/10/19 11:45
+ */
+
+namespace application\modules\main\cli;
+
+
+use application\modules\main\model\Shop;
+use application\modules\main\model\ShopTodoList;
+use application\modules\main\model\ShopTodoToptic;
+use cloud\core\cli\Controller;
+
+class TodoController extends Controller
+{
+    public function actionScws(){
+        $criteria = new \CDbCriteria();
+        $criteria->addCondition("status='1'");
+        $shops = Shop::model()->fetchAll($criteria);
+        $logdate = date("Y-m-d");
+        foreach($shops as $shop){
+            $todos = ShopTodoList::fetchRangeListByNick($shop["nick"],date("Y-m-d",strtotime("-7 days")),date("Y-m-d",strtotime("7 days")));
+            if(empty($todos)){
+                continue;
+            }
+
+            $text = "";
+            foreach($todos as $todo){
+                $text.=$todo["content"];
+            }
+
+            $so = scws_new();
+
+            $so->send_text($text);
+            $result = $so->get_tops(5);
+            $so->close();
+
+            $attrs = array("nick"=>$shop["nick"],"logdate"=>$logdate,"data"=>\CJSON::encode($result));
+            ShopTodoToptic::model()->deleteAll("logdate=? AND nick=?",array($logdate,$shop["nick"]));
+            $model = new ShopTodoToptic();
+            $model->setAttributes($attrs);
+            if(!$model->save()){
+                print_r($model->getErrors());
+            }
+        }
+    }
+
+}
