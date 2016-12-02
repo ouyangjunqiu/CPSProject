67cb704ae028e71ea2e10df2ad084dbba2fc27f1
oShine <oyjqdlp@126.com>
2016-12-02 14:30:30 +0800

    v2.0.0.1

--- system/modules/main/widgets/views/shopstoplist.php
+++ system/modules/main/widgets/views/shopstoplist.php
@@ -3,8 +3,7 @@
     <div class="tab">
         <ul class="nav nav-tabs shop-nav" role="tablist">
             <li role="presentation" class="active"><a href="#home_<?php echo $row["id"];?>" title="基本信息" aria-controls="home_<?php echo $row["id"];?>" role="tab" data-toggle="tab"><i class="glyphicon glyphicon-home"></i><span>基本信息</span></a></li>
-            <li role="presentation"><a href="#op_<?php echo $row["id"];?>" title="店铺操作" aria-controls="op_<?php echo $row["id"];?>" role="tab" data-toggle="tab"><i class="glyphicon glyphicon-cog"></i><span>店铺操作</span></a></li>
-        </ul>
+       </ul>
 
         <div class="tab-content">
             <div role="tabpanel" class="tab-pane active" id="home_<?php echo $row["id"];?>" data-role="shop-base-info">
@@ -77,33 +76,14 @@
                             </div>
                         </div>
 
+                        <div class="row">
+                            <div class="col-md-12">
+                                <a href="javascript:void(0)" class="btn btn-danger" data-click="restart" data-nick="<?php echo $row["nick"];?>" data-url="<?php echo Yii::app()->urlManager->createUrl('/main/shop/restart');?>">恢复服务</a>
+                            </div>
+                        </div>
+
                     </div>
                 </form>
-            </div>
-            <div role="tabpanel" class="tab-pane" id="op_<?php echo $row["id"];?>" data-role="shop-op">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <p class="babyinfor-frame-tit">
-                                <a href="<?php echo $row["shopurl"];?>" target="_blank"><?php echo $row["nick"];?> </a>
-                                <?php if(!empty($row["shopcatname"])):?>
-                                    <o class="tit"><?php echo $row["shopcatname"];?></o>
-                                <?php endif;?>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <small> <strong>店铺相关</strong></small>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <a href="javascript:void(0)"  data-click="restart" data-nick="<?php echo $row["nick"];?>" data-url="<?php echo Yii::app()->urlManager->createUrl('/main/shop/restart');?>"><small>1. 恢复服务..</small></a>
-                        </div>
-                    </div>
-
-                </div>
             </div>
 
         </div>
